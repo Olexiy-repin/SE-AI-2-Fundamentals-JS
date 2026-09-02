330c64da941ed315d6d32baaef65ade9072f98e0
Olexiy-repin <repinlyoha@gmail.com>
2026-09-02 19:52:47 +0300

    classwork

--- js/06-templating.js
+++ js/06-templating.js
@@ -1,4 +1,11 @@
 //* Різниця між властивостями textContent та innerHTML
+// const refs = {
+//   mainTitle: document.querySelector('.js-title'),
+// };
+
+// refs.mainTitle.textContent = '<span>Main</span> title!';
+// refs.mainTitle.innerHTML = '<span>Main</span> title!';
+
 const pictures = [
   {
     width: 700,
@@ -41,8 +48,23 @@ const pictures = [
 */
 
 //* Функція для створення карточки createGalleryCard(cardInfo)
-const createGalleryCard = pictureInfo => {};
+const createGalleryCardTemplate = pictureInfo => {
+  return `
+  <li class="gallery-item">
+    <a href="#">
+      <img src="${pictureInfo.url}" alt="${pictureInfo.alt}" width="${pictureInfo.width}" height="${pictureInfo.height}">
+    </a>
+  </li>
+  `;
+};
 
 //* Створення масиву рядків із елементами
+const galleryCardsTemplate = pictures.map(pic => createGalleryCardTemplate(pic)).join('');
 
 //* Вставка елементів на сторінку (innerHTML/insertAdjacentHTML)
+const refs = {
+  galleryList: document.querySelector('.js-gallery'),
+};
+
+// refs.galleryList.innerHTML = galleryCardsTemplate;
+refs.galleryList.insertAdjacentHTML('beforeend', galleryCardsTemplate);
